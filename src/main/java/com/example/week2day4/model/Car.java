7cf6393e46f8479438b3d63b3de30ec71a996720
alialabdrabalrasol <alialabdrabalrasol@gmail.com>
2022-05-26 09:26:25 +0300

    Homework updated

--- src/main/java/com/example/week2day4/model/Car.java
+++ src/main/java/com/example/week2day4/model/Car.java
@@ -3,16 +3,14 @@ package com.example.week2day4.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.*;
 
 @AllArgsConstructor @Data
 public class Car {
     @NotEmpty(message = "please enter an id")
     private String id;
     @NotEmpty(message = "please enter a car type")
+    @Pattern(regexp = "(Sedan |SUV| Truck)")
     private String carType;
     @NotNull(message = "please enter a price")
     @Positive(message = "car price should be greater than 0")
